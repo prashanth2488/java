c6900cc1e9e2ddc4cfa48857d1cbe7d9493a8672
prashanth <nocturnaldevops@gmail.com>
2024-07-24 19:22:40 +0530

    new change by developer1

--- Example.java
+++ Example.java
@@ -5,5 +5,6 @@ public static void main(String[] args)
 System.out.println("Hello world");
 System.out.println("welcome to DevOps Sessions");
 System.out.println("This is a line added by developer2");
+System.out.print("hello there")
 }
 }
